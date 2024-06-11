e53017948b5018b8e6623fbe0d9ca7735e08aa2b
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-11 14:30:50 +0300

    fix

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,35 +1,19 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"time"
+	"sync"
 )
 
 func main() {
-	ctx := context.Background()
-
-	thingCtx, _ := context.WithTimeout(ctx, 2*time.Second)
-
-	fmt.Println(
-		doTheThing(thingCtx),
-	)
-}
-
-func doTheThing(ctx context.Context) int {
-	ch := make(chan int)
-
-	go func() {
-		time.Sleep(time.Second * 5)
-		ch <- 1
-	}()
-
-	select {
-	case v := <-ch:
-		return v
-	case <-ctx.Done():
-		return 0
+	wg := sync.WaitGroup{}
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(index int, wgr *sync.WaitGroup) {
+			fmt.Println(index)
+			wgr.Done()
+		}(i, &wg)
 	}
-}
 
-//Task: context if done  == 0 else res
+	wg.Wait()
+}

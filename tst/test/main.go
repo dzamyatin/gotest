a3ad7b1395b6d9047a04b97d7f8ea9edaaa3d69a
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-13 09:16:51 +0300

    tst

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 )
 
@@ -32,23 +31,63 @@ func main() {
 
 	//var err error
 
-	//fmt.Println(retErr() == nil)
+	//fmt.Println(retCustomErr() == nil)
+	//fmt.Println(reflect.TypeOf(retCustomErr()))
+	//fmt.Println(reflect.TypeOf(nil))
 	//fmt.Println(errors.Is(retErr(), MyErr{}))
 
-	fmt.Println(errors.Is(CustomErr{err: MyErr{}}, MyErr{}))
-
-	var e error
-	fmt.Println(errors.As(CustomErr{err: MyErr{}}, &e))
+	//fmt.Println(errors.Is(CustomErr{err: MyErr{}}, MyErr{}))
+	//
+	//var e error
+	//fmt.Println(errors.As(CustomErr{err: MyErr{}}, &e))
+	//
+	//fmt.Println(errors.As(CustomErr{err: MyErr{}}, &CustomErr{}))
 
 	//ch := make(chan int, 1)
 	//<-ch //deadlock
 	//ch <- 1
 	//deferFunc()
 	//fmt.Println(panicFunc())
+
+	s := siren{
+		sval: 1,
+		graps: graps{
+			gval: 2,
+		},
+	}
+	fmt.Println(
+		s.test(),
+	)
 }
+
+type siren struct {
+	graps
+	sval int
+}
+
+type graps struct {
+	gval int
+}
+
+func (g graps) test() int {
+	return g.gval
+}
+
+//func (s siren) test2() int {
+//	return s.graps.test()
+//}
+//
+//func (s siren) test() int {
+//	return s.sval
+//}
 
 func retErr() error {
 	return MyErr{}
+}
+
+func retCustomErr() error {
+	var err *MyErr
+	return err
 }
 
 func deferFunc() {

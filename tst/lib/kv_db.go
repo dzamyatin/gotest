0f519e40e46a4730ce64afb307c660c88af4c003
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-18 16:06:15 +0300

    kv db async map

--- tst/lib/kv_db.go
+++ tst/lib/kv_db.go
@@ -7,41 +7,63 @@ import (
 	"time"
 )
 
-//
 //func main() {
 //	wg := sync.WaitGroup{}
 //
-//	db := NewKVDB()
-//	cache := NewKVCache(db)
+//	db := lib.NewKVDB()
+//
+//	cache := lib.NewKVCache(db)
+//	//cache := lib.NewKVDBAsyncMap(db)
+//
+//	var klist [100]string
+//
+//	for i := 0; i < len(klist); i++ {
+//		klist[i] = "tst_" + strconv.Itoa(i)
+//	}
 //
 //	for i := 0; i < 100; i++ {
 //		go func(i int) {
-//			cache.Set("Hella", strconv.Itoa(i))
+//			cache.Set(getListEl(klist, i), strconv.Itoa(i))
 //		}(i)
 //	}
 //
 //	strt := time.Now()
 //	for i := 0; i < 1000000; i++ {
-//		//for i := 0; i < 10; i++ {
 //		wg.Add(1)
-//		go func() {
-//			cache.Get("Hella")
-//			//fmt.Println(
-//			//	cache.Get("Hella"),
-//			//)
+//		go func(ix int) {
+//			ix = getIx(ix, len(klist))
+//			cache.Get(getListEl(klist, i))
 //			wg.Done()
-//		}()
+//		}(i)
 //	}
 //
 //	wg.Wait()
 //
 //	log.Println("Met:")
-//	log.Println(KvMetric.Load())
+//	log.Println(lib.KvMetric.Load())
 //	log.Println("Met2:")
-//	log.Println(KvMetric2.Load())
+//	log.Println(lib.KvMetric2.Load())
 //	end := time.Now().Sub(strt)
 //	fmt.Printf("Time: %s", end)
 //}
+
+func GetListEl(klist [100]string, i int) string {
+	return klist[GetIx(i, len(klist))]
+}
+
+func GetIx(ix int, len int) int {
+	if ix < len {
+		return ix
+	}
+
+	for {
+		ix = ix - len
+
+		if ix < len {
+			return ix
+		}
+	}
+}
 
 var KvMetric = atomic.Int32{}
 var KvMetric2 = atomic.Int32{}

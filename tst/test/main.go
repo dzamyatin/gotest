1ef9894f17b621f4d06c4a32e4cd4c6358804194
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-19 07:44:22 +0300

    unsafe swap pointers

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,2 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"sync/atomic"
+	"unsafe"
+)
+
+func main() {
+	a := 1
+	b := 2
+
+	pa := unsafe.Pointer(&a)
+	pb := unsafe.Pointer(&b)
+
+	atomic.SwapPointer(&pa, pb)
+
+	v := *(*int)(pa)
+
+	fmt.Printf("%v", v)
+
+}

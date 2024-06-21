607343c6bf9483a66506115dbd6f9af46b58a4ef
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-21 16:36:09 +0300

    err group

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,86 +1,33 @@
 package main
 
-import "log"
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	_ "golang.org/x/sync/errgroup"
+	"log"
+	"time"
+)
 
 func main() {
-	tst := 1
-	f := func(tst int) {
-		log.Println(tst)
+	ctx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	group, _ := errgroup.WithContext(ctx)
+	//group := errgroup.Group{}
+
+	group.SetLimit(10)
+
+	for i := 0; i < 100; i++ {
+		group.Go(func() error {
+			select {
+			case <-ctx.Done():
+				return nil
+			default:
+				log.Println(i)
+				time.Sleep(5 * time.Second)
+				return fmt.Errorf("asd")
+			}
+		})
 	}
-	defer f(tst)
-	tst = 2
 
-	//arr := [3]int{1, 2, 3}
-	//slice := []int{1, 2, 3}
-	//m := map[int]int{1: 1, 2: 2, 3: 3}
-	//
-	//e(arr, slice, m)
-	//
-	//t := [3]int(slice)
-	//
-	//tst(t[:]...)
-	//
-	//fmt.Println([3]int(slice) == arr) //true
-	//fmt.Println(slice == slice) //build failed
-	//fmt.Println(m == m) //build failed
-
-	//p := Person{Name: "Alice", Age: 30}
-	//p2 := Person{Name: "Alice", Age: 30}
-	//e := Employee{Name: "Alice", Age: 30, ID: 123}
-
-	//log.Println(p == p2)
-
-	//var i interface{} = &p
-	//change2(&i)
-
-	//change4(&p)
-
-	//log.Println(p.Age)
-}
-
-//type IPers interface {
-//	setAge(age int)
-//}
-//
-//type Person struct {
-//	Name string
-//	Age  int
-//	//Ptr  []int
-//	//Ptr map[int]int
-//}
-//
-//func (p *Person) setAge(age int) {
-//	p.Age = age
-//}
-//
-//type Employee struct {
-//	Name string
-//	Age  int
-//	ID   int
-//}
-//
-//func tst(a ...int) {
-//	fmt.Println(a)
-//}
-//func change(p Person) {
-//	p.Age = 31
-//}
-//
-//func change2(p *interface{}) {
-//	t := (*p).(*Person)
-//
-//	t.setAge(31)
-//}
-//
-//func change3(p IPers) {
-//	p.setAge(31)
-//}
-//
-//func change4(p IPers) {
-//	g := p.(*Person)
-//	g.setAge(31)
-//}
-
-func e(...interface{}) {
-
+	group.Wait()
 }

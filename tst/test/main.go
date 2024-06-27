862ff53242594b8ce417cb46b5e4acb3ce690aa0
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-27 11:46:10 +0300

    tst

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	_ "golang.org/x/sync/errgroup"
 	"log"
 	"sort"
@@ -9,7 +10,11 @@ import (
 )
 
 func main() {
-	ExpSum(4)
+	fmt.Println(
+		ExpSum(4), //5
+		ExpSum(5), //7
+		ExpSum(10),
+	)
 }
 
 // 4
@@ -26,12 +31,21 @@ func ExpSum(n uint64) uint64 {
 	var resultMap = make(map[string]struct{})
 
 	for i := uint64(1); i <= uint64(len(nums)); i++ {
-		resultMap[createKey(elementByPartition(nums, i))] = struct{}{}
+		for j := uint64(1); j <= uint64(len(nums)); j++ {
+			for t := uint64(1); t <= uint64(len(nums)); t++ {
+				resultMap[createKey(elementByPartition(nums, i, j, t-1))] = struct{}{}
+			}
+		}
+		for j := uint64(1); j <= uint64(len(nums)); j++ {
+			for t := uint64(1); t <= uint64(len(nums)); t++ {
+				resultMap[createKey(elementByPartition(nums, i, j, t-1))] = struct{}{}
+			}
+		}
 	}
 
 	log.Println(resultMap)
 
-	return 1
+	return uint64(len(resultMap))
 }
 
 func createKey(nums []uint64) string {
@@ -40,21 +54,48 @@ func createKey(nums []uint64) string {
 	b := strings.Builder{}
 	for _, v := range nums {
 		b.WriteString(strconv.FormatUint(v, 10))
+		b.WriteString(",")
 	}
 
 	return b.String()
 }
 
-func elementByPartition(nums []uint64, partition uint64) (res []uint64) {
+func elementByPartition(
+	nums []uint64,
+	partition uint64,
+	shift uint64,
+	gap uint64,
+) (res []uint64) {
 	var buf []uint64
-	for k, i := range nums {
+	var outbuf []uint64
+	var puf uint64
+	var curGap = gap
+	for _, i := range nums {
+		//for _, i := range nums {
+
+		if gap == 0 || curGap == 0 {
+			//if gap == 0 || gap%uint64(k+1) == 0 {
+			puf++
+			curGap = gap
+		} else {
+			outbuf = append(outbuf, i)
+			curGap--
+			continue
+		}
+
 		buf = append(buf, i)
-		if uint64(k+1)%partition == 0 {
+		r := puf + shift + 1
+
+		if r%partition == 0 {
 			res = append(res, sum(buf))
 			buf = []uint64{}
 		}
+
+		//}
 	}
+
 	res = append(res, buf...)
+	res = append(res, outbuf...)
 	return
 }
 

ca4bf1c29ad8e522dc702a577a11a487df41d3a8
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-06-28 10:26:48 +0300

    cw

--- tst/test/main.go
+++ tst/test/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"fmt"
 	_ "golang.org/x/sync/errgroup"
+	"strings"
 )
 
-type Test[T any] struct {
-	v T
-}
-
-func (t Test[T]) get() T {
-	return t.v
-}
-
 func main() {
-	t := Test[string]{}
-	println(
-		t.get(),
+	fmt.Println(
+		duplicate_count("abcde"),
+		duplicate_count("abcdea"),
+		duplicate_count("abcdeaB11"),
+		duplicate_count("indivisibility"),
 	)
+}
+
+func duplicate_count(s1 string) int {
+	s1 = strings.ToLower(s1)
+	r := []rune(s1)
+	m := make(map[rune]int, len(r))
+
+	res := 0
+	for _, v := range r {
+		if c, ok := m[v]; ok && c == 1 {
+			res++
+		}
+		m[v]++
+	}
+
+	return res
 }

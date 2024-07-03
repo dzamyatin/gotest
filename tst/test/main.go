873a4f82d174aac235a782dd264d720ec5fb6b61
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-07-03 23:28:50 +0300

    cw s3 optimized

--- tst/test/main.go
+++ tst/test/main.go
@@ -10,8 +10,9 @@ import (
 func main() {
 	//fmt.Println(Partitions(1))
 	//fmt.Println(Partitions(5))
-	fmt.Println(Partitions(10))
+	//fmt.Println(Partitions(10))
 	//fmt.Println(Partitions(25))
+	fmt.Println(Partitions(40))
 	//fmt.Println(Partitions(100))
 	//
 	//fmt.Println("ST", stat)
@@ -103,69 +104,3 @@ func (p *Part) overfull(r []int) bool {
 }
 
 ///---------------------------------------------
-
-var cyclePath = make(map[string]int)
-
-func Tst(n int) int {
-
-	var sum int
-	var storage = make(map[string]struct{})
-
-	m := make([]int, n)
-
-	for i := 0; i < n; i++ {
-		m[i] = i + 1
-	}
-
-	fmt.Println(m)
-
-	rec(m, 0, []int{}, &sum, storage)
-
-	return sum
-}
-
-func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
-	//stat++
-	for _, v := range m {
-		newPath := make([]int, len(path))
-
-		copy(newPath, path)
-		newPath = append(newPath, v)
-
-		if overfull(newPath) {
-			continue
-		}
-
-		if cur+v == len(m) {
-			store(newPath, sum, storage)
-			continue
-		}
-		if cur+v < len(m) {
-			rec(m, cur+v, newPath, sum, storage)
-		}
-	}
-}
-
-func overfull(r []int) bool {
-	//fmt.Println(r)
-
-	sort.Ints(r)
-	k := fmt.Sprint(r)
-
-	v := cyclePath[k]
-
-	cyclePath[k]++
-
-	return v > 0
-}
-
-func store(r []int, sum *int, storage map[string]struct{}) {
-	sort.Ints(r)
-	k := fmt.Sprint(r)
-	if _, ok := storage[k]; ok {
-		return
-	}
-	//fmt.Println(r)
-	storage[k] = struct{}{}
-	*sum++
-}

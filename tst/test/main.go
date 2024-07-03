520f7c4c4cd79c3e6371c0a42af70c9ace00bc72
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-07-03 23:26:09 +0300

    cw s3 optimized

--- tst/test/main.go
+++ tst/test/main.go
@@ -5,15 +5,16 @@ import (
 	"sort"
 )
 
-var stat int
+//var stat int
 
 func main() {
 	//fmt.Println(Partitions(1))
 	//fmt.Println(Partitions(5))
-	//fmt.Println(Partitions(10))
-	fmt.Println(Partitions(25))
+	fmt.Println(Partitions(10))
+	//fmt.Println(Partitions(25))
+	//fmt.Println(Partitions(100))
 	//
-	fmt.Println("ST", stat)
+	//fmt.Println("ST", stat)
 }
 
 // [5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]
@@ -24,9 +25,88 @@ func main() {
 // [1,1,1,1,1] => 4-1,
 // [1,1,1,1,1] => 5-1,
 
+func Partitions(n int) int {
+	np := NewPart(n)
+	return np.run()
+}
+
+type Part struct {
+	sum     int
+	storage map[string]struct{}
+	cycle   map[string]struct{}
+	number  int
+}
+
+func NewPart(number int) Part {
+	return Part{
+		storage: make(map[string]struct{}),
+		cycle:   make(map[string]struct{}),
+		number:  number,
+	}
+}
+
+func (p *Part) run() int {
+	m := make([]int, p.number)
+
+	for i := 0; i < p.number; i++ {
+		m[i] = i + 1
+	}
+
+	p.rec(m, 0, []int{})
+
+	return p.sum
+}
+
+func (p *Part) rec(m []int, cur int, path []int) {
+	for _, v := range m {
+		newPath := make([]int, len(path))
+
+		copy(newPath, path)
+		newPath = append(newPath, v)
+
+		if p.overfull(newPath) {
+			continue
+		}
+
+		if cur+v == len(m) {
+			p.store(newPath)
+			continue
+		}
+		if cur+v < len(m) {
+			p.rec(m, cur+v, newPath)
+		}
+	}
+}
+
+func (p *Part) store(r []int) {
+	sort.Ints(r)
+	k := fmt.Sprint(r)
+	if _, ok := p.storage[k]; ok {
+		return
+	}
+	//fmt.Println(r)
+	p.storage[k] = struct{}{}
+	p.sum++
+}
+
+func (p *Part) overfull(r []int) bool {
+	sort.Ints(r)
+	k := fmt.Sprint(r)
+
+	if _, ok := p.cycle[k]; ok {
+		return true
+	}
+
+	p.cycle[k] = struct{}{}
+
+	return false
+}
+
+///---------------------------------------------
+
 var cyclePath = make(map[string]int)
 
-func Partitions(n int) int {
+func Tst(n int) int {
 
 	var sum int
 	var storage = make(map[string]struct{})
@@ -45,7 +125,7 @@ func Partitions(n int) int {
 }
 
 func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
-	stat++
+	//stat++
 	for _, v := range m {
 		newPath := make([]int, len(path))
 
@@ -67,7 +147,7 @@ func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
 }
 
 func overfull(r []int) bool {
-	fmt.Println(r)
+	//fmt.Println(r)
 
 	sort.Ints(r)
 	k := fmt.Sprint(r)

7ac9db6c5b5221fafecc060957a1ee0586653c1c
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-07-03 22:55:49 +0300

    cw s3

--- tst/test/main.go
+++ tst/test/main.go
@@ -2,105 +2,65 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"sort"
 )
 
-var stat = 0
-
 func main() {
 	//fmt.Println(Partitions(1))
-	fmt.Println(Partitions(5))
-	//fmt.Println(Partitions(10))
+	//fmt.Println(Partitions(5))
+	fmt.Println(Partitions(10))
 	//fmt.Println(Partitions(25))
-
-	fmt.Printf("ST:%v", stat)
 }
 
 // [5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]
-// [1,1,1,1,1] 0/1 => [1,1,1,1,1] //
-// [1,1,1,1,1] 0/2 => [2,2,1] // 1/2 [2,3]
-// [1,1,1,1,1] 0/3 => [3,1,1] // 1/2 [3,2]
-// [1,1,1,1,1] 0/4 => [4,1]
-// [1,1,1,1,1] 0/5 => [5]
+//
+// [1,1,1,1,1] => 1-5
+// [1,1,1,1,1] => 2-2,
+// [1,1,1,1,1] => 3-1,
+// [1,1,1,1,1] => 4-1,
+// [1,1,1,1,1] => 5-1,
+
 func Partitions(n int) int {
 
-	m := make(map[string]struct{})
+	var sum int
+	var storage = make(map[string]struct{})
 
-	r := make([]int, n)
-	for i := range r {
-		r[i] = 1
+	m := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		m[i] = i + 1
 	}
 
-	//fmt.Println(
-	//	conv(r, 1, 0),
-	//	conv(r, 2, 0),
-	//	conv(r, 3, 0),
-	//	conv(r, 4, 0),
-	//	conv(r, 5, 0),
-	//
-	//	conv(r, 1, 1),
-	//	conv(r, 2, 1),
-	//	conv(r, 3, 1),
-	//	conv(r, 4, 1),
-	//	conv(r, 5, 1),
-	//)
+	fmt.Println(m)
 
-	rec(r, m, 0, true)
+	rec(m, 0, []int{}, &sum, storage)
 
-	log.Println(m)
-
-	return len(m)
+	return sum
 }
 
-func rec(elem []int, m map[string]struct{}, g int, flag bool) {
-	stat++
-	for i := 1; i <= len(elem); i++ {
-		r := conv(elem, i, g)
-		store(r, m)
+func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
+	for _, v := range m {
+		newPath := make([]int, len(path))
+		copy(newPath, path)
+		newPath = append(newPath, v)
 
-		if len(r)+g >= i && (len(r) != len(elem) || flag) {
-			for j := 0; j <= len(elem); j++ {
-				rec(r, m, j, false)
-			}
-		}
-	}
-}
-
-func conv(elem []int, group int, gap int) []int {
-	var r []int
-	j := 0
-
-	var buf []int
-	for i := 0; i < len(elem); i++ {
-
-		if i >= gap {
-			j++
-		}
-		buf = append(buf, elem[i])
-		if j%group == 0 {
-			r = append(r, sum(buf))
-			buf = []int{}
+		if cur+v == len(m) {
+			store(newPath, sum, storage)
 			continue
 		}
+		if cur+v < len(m) {
+			rec(m, cur+v, newPath, sum, storage)
+		}
 	}
-
-	if sum(buf) > 0 {
-		r = append(r, sum(buf))
-	}
-
-	return r
 }
 
-func sum(ele []int) int {
-	var s int
-	for i := 0; i < len(ele); i++ {
-		s += ele[i]
-	}
-	return s
-}
-
-func store(r []int, m map[string]struct{}) {
+func store(r []int, sum *int, storage map[string]struct{}) {
 	sort.Ints(r)
-	m[fmt.Sprint(r)] = struct{}{}
+	k := fmt.Sprint(r)
+	if _, ok := storage[k]; ok {
+		return
+	}
+	//fmt.Println(r)
+	storage[k] = struct{}{}
+	*sum++
 }

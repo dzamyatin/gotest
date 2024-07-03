a0b62af096217adc79a59526922a7a6de699b868
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-07-04 00:50:42 +0300

    try to optimuze

--- tst/test/main.go
+++ tst/test/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
 	"sort"
 	"time"
 )
@@ -9,17 +12,32 @@ import (
 //var stat int
 
 func main() {
+	f, _ := os.Create("cpu.prof")
+	err := pprof.StartCPUProfile(f)
+	if err != nil {
+		log.Fatal("Write cpu profiling started error")
+	}
+
+	fs, _ := os.Create("heap.prof")
+	err = pprof.WriteHeapProfile(fs)
+	if err != nil {
+		log.Fatal("Write heap profiling started error")
+	}
+
 	t := time.Now()
 	//fmt.Println(Partitions(1))
 	//fmt.Println(Partitions(5))
 	//fmt.Println(Partitions(10))
-	fmt.Println(Partitions(25))
-	//fmt.Println(Partitions(40))
+	//fmt.Println(Partitions(25))
+	fmt.Println(Partitions(40))
+	//fmt.Println(Partitions(50))
 	//fmt.Println(Partitions(100))
 
 	fmt.Println("ST", time.Now().Sub(t))
+	pprof.StopCPUProfile()
 	//
 	//fmt.Println("ST", stat)
+
 }
 
 // [5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]
@@ -83,7 +101,13 @@ func (p *Part) rec(m []int, cur int, path []int) {
 
 func (p *Part) overfull(r []int) bool {
 	sort.Ints(r)
-	k := fmt.Sprint(r)
+
+	var b []byte
+	for _, v := range r {
+		b = append(b, byte(v))
+	}
+
+	k := string(b)
 
 	if _, ok := p.cycle[k]; ok {
 		return true
@@ -93,5 +117,36 @@ func (p *Part) overfull(r []int) bool {
 
 	return false
 }
+
+//func (p *Part) overfull(r []int) bool {
+//	sort.Ints(r)
+//
+//	b := strings.Builder{}
+//	for _, v := range r {
+//		b.WriteByte(byte(v))
+//	}
+//	k := b.String()
+//
+//	if _, ok := p.cycle[k]; ok {
+//		return true
+//	}
+//
+//	p.cycle[k] = struct{}{}
+//
+//	return false
+//}
+
+//func (p *Part) overfull(r []int) bool {
+//	sort.Ints(r)
+//	k := fmt.Sprint(r)
+//
+//	if _, ok := p.cycle[k]; ok {
+//		return true
+//	}
+//
+//	p.cycle[k] = struct{}{}
+//
+//	return false
+//}
 
 ///---------------------------------------------

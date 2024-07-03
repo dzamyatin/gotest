8436c532eb52f5d9609150ae4f8de8e9cc81b9d2
Даниил Замятин <d.zamyatin@dionsoft.ru>
2024-07-03 23:11:30 +0300

    cw s3 optimized

--- tst/test/main.go
+++ tst/test/main.go
@@ -5,11 +5,15 @@ import (
 	"sort"
 )
 
+var stat int
+
 func main() {
 	//fmt.Println(Partitions(1))
 	//fmt.Println(Partitions(5))
-	fmt.Println(Partitions(10))
-	//fmt.Println(Partitions(25))
+	//fmt.Println(Partitions(10))
+	fmt.Println(Partitions(25))
+	//
+	fmt.Println("ST", stat)
 }
 
 // [5], [4,1], [3,2], [3,1,1], [2,2,1], [2,1,1,1], [1,1,1,1,1]
@@ -19,6 +23,8 @@ func main() {
 // [1,1,1,1,1] => 3-1,
 // [1,1,1,1,1] => 4-1,
 // [1,1,1,1,1] => 5-1,
+
+var cyclePath = make(map[string]int)
 
 func Partitions(n int) int {
 
@@ -39,10 +45,16 @@ func Partitions(n int) int {
 }
 
 func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
+	stat++
 	for _, v := range m {
 		newPath := make([]int, len(path))
+
 		copy(newPath, path)
 		newPath = append(newPath, v)
+
+		if overfull(newPath) {
+			continue
+		}
 
 		if cur+v == len(m) {
 			store(newPath, sum, storage)
@@ -52,6 +64,19 @@ func rec(m []int, cur int, path []int, sum *int, storage map[string]struct{}) {
 			rec(m, cur+v, newPath, sum, storage)
 		}
 	}
+}
+
+func overfull(r []int) bool {
+	fmt.Println(r)
+
+	sort.Ints(r)
+	k := fmt.Sprint(r)
+
+	v := cyclePath[k]
+
+	cyclePath[k]++
+
+	return v > 0
 }
 
 func store(r []int, sum *int, storage map[string]struct{}) {
